63c959673de073ba06254cff6c8694a1ebaacf85
Lucas Leow <leowjianhoong@gmail.com>
2023-06-08 14:01:03 +0800

    Set up routes

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.port;
 
+app.use('/api/contacts', require("./routes/contactRoutes"));
 
 app.listen(port, ()=>{
     console.log(`Server running on Port ${port}`);
